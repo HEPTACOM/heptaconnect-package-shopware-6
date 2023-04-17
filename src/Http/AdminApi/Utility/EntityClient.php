ab1d9ed84f394d560eb640b75f107609bc3b3033
Joshua Behrens <behrens@heptacom.de>
2023-04-17 19:04:28 +0200

    Add entity client

---
+++ src/Http/AdminApi/Utility/EntityClient.php
@@ -0,0 +1,234 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Utility;
+
+use Heptacom\HeptaConnect\Package\Shopware6\Contract\Criteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Contract\Entity;
+use Heptacom\HeptaConnect\Package\Shopware6\Contract\EntityCollection;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityCreate\EntityCreateActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityCreate\EntityCreatePayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityDelete\EntityDeleteActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityDelete\EntityDeleteCriteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityGet\EntityGetActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityGet\EntityGetCriteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearch\EntitySearchActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearch\EntitySearchCriteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearchId\EntitySearchIdActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearchId\EntitySearchIdCriteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityUpdate\EntityUpdateActionInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityUpdate\EntityUpdatePayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\Exception\ResourceNotFoundException;
+use Heptacom\HeptaConnect\Package\Shopware6\Support\LetterCase;
+
+/**
+ * Facade to entity action methods with a reduced set of arguments.
+ *
+ * @final If you want to intercept a process, use the action services.
+ */
+class EntityClient
+{
+    private EntitySearchActionInterface $searchAction;
+
+    private EntitySearchIdActionInterface $searchIdAction;
+
+    private EntityCreateActionInterface $createAction;
+
+    private EntityGetActionInterface $getAction;
+
+    private EntityUpdateActionInterface $updateAction;
+
+    private EntityDeleteActionInterface $deleteAction;
+
+    public function __construct(
+        EntitySearchActionInterface $searchAction,
+        EntitySearchIdActionInterface $searchIdAction,
+        EntityCreateActionInterface $createAction,
+        EntityGetActionInterface $getAction,
+        EntityUpdateActionInterface $updateAction,
+        EntityDeleteActionInterface $deleteAction
+    ) {
+        $this->searchAction = $searchAction;
+        $this->searchIdAction = $searchIdAction;
+        $this->createAction = $createAction;
+        $this->getAction = $getAction;
+        $this->updateAction = $updateAction;
+        $this->deleteAction = $deleteAction;
+    }
+
+    /**
+     * Searches for entities.
+     *
+     * @throws \Throwable
+     */
+    public function search(string $entityName, ?Criteria $criteria = null): EntityCollection
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+        $criteria ??= new Criteria();
+
+        return $this->searchAction->search(new EntitySearchCriteria($entityName, $criteria))->getData();
+    }
+
+    /**
+     * Searches for entity ids.
+     *
+     * @throws \Throwable
+     *
+     * @returns list<string>|list<array<string, string>>
+     */
+    public function searchIds(string $entityName, ?Criteria $criteria = null): array
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+        $criteria ??= new Criteria();
+
+        return $this->searchIdAction->searchIds(new EntitySearchIdCriteria($entityName, $criteria))->getData();
+    }
+
+    /**
+     * Counts the entity by the given criteria.
+     *
+     * @throws \Throwable
+     */
+    public function count(string $entityName, ?Criteria $criteria = null): int
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+        $criteria ??= new Criteria();
+        $criteria = $criteria->withTotalCountMode(Criteria::TOTAL_COUNT_MODE_EXACT);
+        $criteria = $criteria->withLimit(1);
+
+        return $this->searchIdAction->searchIds(new EntitySearchIdCriteria($entityName, $criteria))->getTotal();
+    }
+
+    /**
+     * Iterates over all entities by the given criteria.
+     *
+     * @throws \Throwable
+     *
+     * @return iterable<Entity>
+     */
+    public function iterate(string $entityName, ?Criteria $criteria = null): iterable
+    {
+        $criteria ??= new Criteria();
+        $criteria = $criteria->withTotalCountMode(Criteria::TOTAL_COUNT_MODE_NEXT_PAGES);
+        $criteria = $criteria->withLimit($criteria->getLimit() ?? 500);
+        $criteria = $criteria->withPage(1);
+        $index = 0;
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        do {
+            $searchResult = $this->searchAction->search(new EntitySearchCriteria($entityName, $criteria));
+
+            foreach ($searchResult->getData() as $data) {
+                yield $index++ => $data;
+            }
+
+            $criteria = $criteria->withPage($criteria->getPage() + 1);
+        } while (\count($searchResult->getData()) < $searchResult->getTotal());
+    }
+
+    /**
+     * Iterates over all entities by the given criteria.
+     *
+     * @throws \Throwable
+     *
+     * @return iterable<string|list<string>>
+     */
+    public function iterateIds(string $entityName, ?Criteria $criteria = null): iterable
+    {
+        $criteria ??= new Criteria();
+        $criteria = $criteria->withTotalCountMode(Criteria::TOTAL_COUNT_MODE_NEXT_PAGES);
+        $criteria = $criteria->withLimit($criteria->getLimit() ?? 500);
+        $criteria = $criteria->withPage(1);
+        $index = 0;
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        do {
+            $searchResult = $this->searchIdAction->searchIds(new EntitySearchIdCriteria($entityName, $criteria));
+
+            foreach ($searchResult->getData() as $data) {
+                yield $index++ => $data;
+            }
+
+            $criteria = $criteria->withPage($criteria->getPage() + 1);
+        } while (\count($searchResult->getData()) < $searchResult->getTotal());
+    }
+
+    /**
+     * Reads the first id matched from the criteria.
+     *
+     * @return string|string[]|null
+     */
+    public function getFirstId(string $entityName, Criteria $criteria)
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+        $criteria = $criteria->withLimit(1)->withPage(1);
+        $searchResult = $this->searchIdAction->searchIds(new EntitySearchIdCriteria($entityName, $criteria));
+
+        return $searchResult->getData()[0] ?? null;
+    }
+
+    /**
+     * Checks, whether the entity exists with the given id.
+     */
+    public function exists(string $entityName, string $id): bool
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+        $criteria = (new Criteria())->withLimit(1)->withPage(1)->withIds([$id]);
+        $searchResult = $this->searchIdAction->searchIds(new EntitySearchIdCriteria($entityName, $criteria));
+
+        return $searchResult->getTotal() > 0;
+    }
+
+    /**
+     * Creates the entity and returns the created id.
+     *
+     * @throws \Throwable
+     */
+    public function create(string $entityName, array $payload): string
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        return $this->createAction->create(new EntityCreatePayload($entityName, $payload))->getId();
+    }
+
+    /**
+     * Reads and returns the entity.
+     *
+     * @throws \Throwable
+     */
+    public function get(string $entityName, string $id): Entity
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        return $this->getAction->get(new EntityGetCriteria($entityName, $id, new Criteria()))->getEntity();
+    }
+
+    /**
+     * Updates the entity and returns the created id.
+     *
+     * @throws \Throwable
+     */
+    public function update(string $entityName, array $payload): void
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        $this->updateAction->update(new EntityUpdatePayload($entityName, $payload['id'], $payload));
+    }
+
+    /**
+     * Deletes the entity.
+     *
+     * @throws \Throwable
+     */
+    public function delete(string $entityName, string $id): void
+    {
+        $entityName = LetterCase::fromUnderscoreToDash($entityName);
+
+        try {
+            $this->deleteAction->delete(new EntityDeleteCriteria($entityName, $id));
+        } catch (ResourceNotFoundException $e) {
+            // this is ok, because it is not in the storage anymore
+        }
+    }
+}
---
+++ test/Integration/AdminApi/Utility/EntityClientTest.php
@@ -0,0 +1,172 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Package\Shopware6\Test\Integration\AdminApi\Utility;
+
+use Heptacom\HeptaConnect\Package\Shopware6\Contract\Criteria;
+use Heptacom\HeptaConnect\Package\Shopware6\CriteriaFormatter;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityCreateAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityDeleteAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityGetAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntitySearchAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntitySearchIdAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityUpdateAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\Exception\ResourceNotFoundException;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Utility\EntityClient;
+use Heptacom\HeptaConnect\Package\Shopware6\Test\Integration\AdminApi\Action\AbstractActionTestCase;
+
+/**
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Contract\Criteria
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Contract\Entity
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Contract\EntityCollection
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\CriteriaFormatter
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\AbstractActionClient
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Authentication\ApiConfiguration
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Authentication\AuthenticatedHttpClient
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Authentication\Exception\AuthenticationFailed
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Authentication\PortalNodeStorageAuthenticationStorage
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityCreate\EntityCreatePayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityCreate\EntityCreateResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityDelete\EntityDeleteCriteria
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityDelete\EntityDeleteResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityGet\EntityGetCriteria
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityGet\EntityGetResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearchId\EntitySearchIdCriteria
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearchId\EntitySearchIdResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearch\EntitySearchCriteria
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearch\EntitySearchResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityUpdate\EntityUpdatePayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntityUpdate\EntityUpdateResult
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityCreateAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityDeleteAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityGetAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntitySearchAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntitySearchIdAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\EntityUpdateAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Exception\EntityReferenceLocationFormatInvalidException
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\Exception\AbstractRequestException
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\Exception\ResourceNotFoundException
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseErrorHandler
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\ExpectationFailedValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\FieldIsBlankValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\InvalidLimitQueryValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\InvalidTypeValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\MethodNotAllowedValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\NotFoundValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\ResourceNotFoundValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\ServerErrorValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\ErrorHandling\JsonResponseValidator\WriteTypeIntendErrorValidator
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Support\ExpectedPackagesAwareTrait
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Support\LetterCase
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Utility\EntityClient
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Support\JsonStreamUtility
+ */
+final class EntityClientTest extends AbstractActionTestCase
+{
+    public function testIterateCountries(): void
+    {
+        $client = $this->createEntityClient();
+        $countries = [];
+
+        foreach ($client->iterate('country', (new Criteria())->withLimit(50)) as $country) {
+            $countries[$country->id] = $country;
+        }
+
+        static::assertCount($client->count('country'), $countries);
+
+        $todoCountries = $countries;
+
+        foreach ($client->iterateIds('country', (new Criteria())->withLimit(50)) as $countryId) {
+            static::assertArrayHasKey($countryId, $countries);
+
+            unset($todoCountries[$countryId]);
+        }
+
+        static::assertSame([], $todoCountries);
+
+        $todoCountries = $countries;
+
+        foreach ($client->search('country', (new Criteria())->withIds(\array_keys($countries))) as $country) {
+            static::assertArrayHasKey($country->id, $countries);
+
+            unset($todoCountries[$country->id]);
+        }
+
+        static::assertSame([], $todoCountries);
+
+        $todoCountries = $countries;
+
+        foreach ($client->searchIds('country', (new Criteria())->withIds(\array_keys($countries))) as $countryId) {
+            static::assertArrayHasKey($countryId, $countries);
+
+            unset($todoCountries[$countryId]);
+        }
+
+        static::assertSame([], $todoCountries);
+    }
+
+    public function testGetFirstCountry(): void
+    {
+        $client = $this->createEntityClient();
+        $countryId = $client->getFirstId('country', new Criteria());
+
+        static::assertIsString($countryId);
+
+        $country = $client->get('country', $countryId);
+
+        static::assertSame($countryId, $country->id);
+    }
+
+    public function testThatAnAlreadyDeletedEntryIsNotBadOnSecondTryToDelete(): void
+    {
+        $client = $this->createEntityClient();
+        $tagId = $client->create('tag', [
+            'name' => \bin2hex(\random_bytes(24)),
+        ]);
+
+        $client->delete('tag', $tagId);
+        // this will throw an exception but this is muted
+        $client->delete('tag', $tagId);
+
+        static::assertFalse($client->exists('tag', $tagId));
+        static::expectException(ResourceNotFoundException::class);
+
+        // this exception is not muted
+        $client->get('tag', $tagId);
+    }
+
+    public function testEntityLifecycle(): void
+    {
+        $client = $this->createEntityClient();
+        $tagId = $client->create('tag', [
+            'name' => \bin2hex(\random_bytes(24)),
+        ]);
+
+        static::assertTrue($client->exists('tag', $tagId));
+
+        $newName = \bin2hex(\random_bytes(24));
+        $client->update('tag', [
+            'id' => $tagId,
+            'name' => $newName,
+        ]);
+
+        static::assertSame($newName, $client->get('tag', $tagId)->name);
+
+        $client->delete('tag', $tagId);
+
+        static::assertFalse($client->exists('tag', $tagId));
+    }
+
+    private function createEntityClient(): EntityClient
+    {
+        return new EntityClient(
+            $this->createAction(EntitySearchAction::class, new CriteriaFormatter()),
+            $this->createAction(EntitySearchIdAction::class, new CriteriaFormatter()),
+            $this->createAction(EntityCreateAction::class),
+            $this->createAction(EntityGetAction::class),
+            $this->createAction(EntityUpdateAction::class),
+            $this->createAction(EntityDeleteAction::class)
+        );
+    }
+}
