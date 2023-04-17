94e74a9d392798f083a8486fdf7054a54aa93d36
Joshua Behrens <behrens@heptacom.de>
2023-04-18 01:08:09 +0200

    Extract common code for entity search criteria

---
+++ src/Http/AdminApi/Entity/Contract/AbstractEntitySearchCriteria.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract;
+
+use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
+use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
+use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
+use Heptacom\HeptaConnect\Package\Shopware6\Contract\Criteria;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Contract\ExpectedPackagesAwareInterface;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Support\ExpectedPackagesAwareTrait;
+
+abstract class AbstractEntitySearchCriteria implements AttachmentAwareInterface, ExpectedPackagesAwareInterface
+{
+    use AttachmentAwareTrait;
+    use ExpectedPackagesAwareTrait;
+
+    private string $entityName;
+
+    private Criteria $criteria;
+
+    public function __construct(string $entityName, Criteria $criteria)
+    {
+        $this->attachments = new AttachmentCollection();
+        $this->entityName = $entityName;
+        $this->criteria = $criteria;
+    }
+
+    public function getEntityName(): string
+    {
+        return $this->entityName;
+    }
+
+    /**
+     * @return static
+     */
+    public function withEntityName(string $entityName): self
+    {
+        $that = clone $this;
+        $that->entityName = $entityName;
+
+        return $that;
+    }
+
+    public function getCriteria(): Criteria
+    {
+        return $this->criteria;
+    }
+
+    /**
+     * @return static
+     */
+    public function withCriteria(Criteria $criteria): self
+    {
+        $that = clone $this;
+        $that->criteria = $criteria;
+
+        return $that;
+    }
+}
--- src/Http/AdminApi/Entity/Contract/EntitySearch/EntitySearchCriteria.php
+++ src/Http/AdminApi/Entity/Contract/EntitySearch/EntitySearchCriteria.php
@@ -4,52 +4,8 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\EntitySearch;
 
-use Heptacom\HeptaConnect\Dataset\Base\AttachmentCollection;
-use Heptacom\HeptaConnect\Dataset\Base\Contract\AttachmentAwareInterface;
-use Heptacom\HeptaConnect\Dataset\Base\Support\AttachmentAwareTrait;
-use Heptacom\HeptaConnect\Package\Shopware6\Contract\Criteria;
-use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Contract\ExpectedPackagesAwareInterface;
-use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Support\ExpectedPackagesAwareTrait;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Entity\Contract\AbstractEntitySearchCriteria;
 
-final class EntitySearchCriteria implements AttachmentAwareInterface, ExpectedPackagesAwareInterface
+final class EntitySearchCriteria extends AbstractEntitySearchCriteria
 {
-    use AttachmentAwareTrait;
-    use ExpectedPackagesAwareTrait;
-
-    private string $entityName;
-
-    private Criteria $criteria;
-
-    public function __construct(string $entityName, Criteria $criteria)
-    {
-        $this->attachments = new AttachmentCollection();
-        $this->entityName = $entityName;
-        $this->criteria = $criteria;
-    }
-
-    public function getEntityName(): string
-    {
-        return $this->entityName;
-    }
-
-    public function withEntityName(string $entityName): self
-    {
-        $that = clone $this;
-        $that->entityName = $entityName;
-
-        return $that;
-    }
-
-    public function getCriteria(): Criteria
-    {
-        return $this->criteria;
-    }
-
-    public function withCriteria(Criteria $criteria): self
-    {
-        $that = clone $this;
-        $that->criteria = $criteria;
-
-        return $that;
-    }
 }
