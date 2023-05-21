d628fb25b61213191d55870c7aa4422c6b578f19
Joshua Behrens <behrens@heptacom.de>
2023-05-21 05:11:48 +0200

    Ensure configuration exists by installing a plugin

--- test/Integration/AdminApi/Action/SystemConfigTest.php
+++ test/Integration/AdminApi/Action/SystemConfigTest.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace Heptacom\HeptaConnect\Package\Shopware6\Test\Integration\AdminApi\Action;
 
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionActivate\ExtensionActivatePayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionDeactivate\ExtensionDeactivatePayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionInstall\ExtensionInstallPayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionRemove\ExtensionRemovePayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionUninstall\ExtensionUninstallPayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionUpload\ExtensionUploadPayload;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigBatch\SystemConfigBatchPayload;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigGet\SystemConfigGetCriteria;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigPost\SystemConfigPostPayload;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionActivateAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionDeactivateAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionInstallAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionRemoveAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionUninstallAction;
+use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionUploadAction;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\SystemConfigBatchAction;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\SystemConfigGetAction;
 use Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\SystemConfigPostAction;
@@ -16,12 +28,24 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\AbstractActionClient
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionActivate\ExtensionActivatePayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionDeactivate\ExtensionDeactivatePayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionInstall\ExtensionInstallPayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionRemove\ExtensionRemovePayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionUninstall\ExtensionUninstallPayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\ExtensionUpload\ExtensionUploadPayload
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\InfoVersion\InfoVersionParams
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\InfoVersion\InfoVersionResult
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigBatch\SystemConfigBatchPayload
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigGet\SystemConfigGetCriteria
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigGet\SystemConfigGetResult
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Contract\SystemConfigPost\SystemConfigPostPayload
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionActivateAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionDeactivateAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionInstallAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionRemoveAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionUninstallAction
+ * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\ExtensionUploadAction
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\InfoVersionAction
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\Support\ActionClientUtils
  * @covers \Heptacom\HeptaConnect\Package\Shopware6\Http\AdminApi\Action\SystemConfigBatchAction
@@ -70,6 +94,56 @@ use PHPUnit\Framework\TestCase;
  */
 final class SystemConfigTest extends TestCase
 {
+    private static string $pluginName = '';
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $factory = Factory::createAdminApiFactory();
+        $streamFactory = $factory->getBaseFactory()->getStreamFactory();
+        $upload = new ExtensionUploadAction($factory->getActionClientUtils(), $streamFactory);
+        $install = new ExtensionInstallAction($factory->getActionClientUtils());
+        $activate = new ExtensionActivateAction($factory->getActionClientUtils());
+
+        $extensionName = self::createExtensionName();
+        $upload->uploadExtension(new ExtensionUploadPayload(
+            $extensionName . '.zip',
+            $streamFactory->createStreamFromFile(self::createExtensionFile($extensionName))
+        ));
+        $install->installExtension(new ExtensionInstallPayload('plugin', $extensionName));
+        $activate->activateExtension(new ExtensionActivatePayload('plugin', $extensionName));
+
+        self::$pluginName = $extensionName;
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+
+        $factory = Factory::createAdminApiFactory();
+        $deactivate = new ExtensionDeactivateAction($factory->getActionClientUtils());
+        $uninstall = new ExtensionUninstallAction($factory->getActionClientUtils());
+        $remove = new ExtensionRemoveAction($factory->getActionClientUtils());
+
+        try {
+            $deactivate->deactivateExtension(new ExtensionDeactivatePayload('plugin', self::$pluginName));
+        } catch (\Throwable $_) {
+        }
+
+        try {
+            $uninstall->uninstallExtension(new ExtensionUninstallPayload('plugin', self::$pluginName));
+        } catch (\Throwable $_) {
+        }
+
+        try {
+            $remove->removeExtension(new ExtensionRemovePayload('plugin', self::$pluginName));
+        } catch (\Throwable $_) {
+        }
+
+        self::$pluginName = '';
+    }
+
     public function testReadWriteSingleAndBatchCycle(): void
     {
         $actionClientUtils = Factory::createAdminApiFactory()->getActionClientUtils();
@@ -83,24 +157,24 @@ final class SystemConfigTest extends TestCase
 
         $batchAction->batchSystemConfig(new SystemConfigBatchPayload([
             SystemConfigBatchPayload::GLOBAL_SALES_CHANNEL => [
-                'HeptaConnectPackageShopware6.config.testValue' => 'foobar',
+                self::$pluginName . '.config.testValue' => 'foobar',
             ],
         ]));
 
-        $testSettings = $getAction->getSystemConfig(new SystemConfigGetCriteria('HeptaConnectPackageShopware6.config'))->getValues();
+        $testSettings = $getAction->getSystemConfig(new SystemConfigGetCriteria(self::$pluginName . '.config'))->getValues();
 
         static::assertSame([
-            'HeptaConnectPackageShopware6.config.testValue' => 'foobar',
+            self::$pluginName . '.config.testValue' => 'foobar',
         ], $testSettings);
 
         $postAction->postSystemConfig(new SystemConfigPostPayload([
-            'HeptaConnectPackageShopware6.config.testValue' => 'foobaz',
+            self::$pluginName . '.config.testValue' => 'foobaz',
         ]));
 
-        $testSettings = $getAction->getSystemConfig(new SystemConfigGetCriteria('HeptaConnectPackageShopware6.config'))->getValues();
+        $testSettings = $getAction->getSystemConfig(new SystemConfigGetCriteria(self::$pluginName . '.config'))->getValues();
 
         static::assertSame([
-            'HeptaConnectPackageShopware6.config.testValue' => 'foobaz',
+            self::$pluginName . '.config.testValue' => 'foobaz',
         ], $testSettings);
     }
 
@@ -117,7 +191,7 @@ final class SystemConfigTest extends TestCase
 
         $action->batchSystemConfig(new SystemConfigBatchPayload([
             '00000000000000000000000000000000' => [
-                'HeptaConnectPackageShopware6.config.testValue' => 'invalid',
+                self::$pluginName . '.config.testValue' => 'invalid',
             ],
         ]));
     }
@@ -134,7 +208,7 @@ final class SystemConfigTest extends TestCase
         $action = new SystemConfigPostAction(Factory::createAdminApiFactory()->getActionClientUtils());
 
         $action->postSystemConfig(new SystemConfigPostPayload([
-            'HeptaConnectPackageShopware6.config.testValue' => 'invalid',
+            self::$pluginName . '.config.testValue' => 'invalid',
         ], '00000000000000000000000000000000'));
     }
 
@@ -161,13 +235,85 @@ final class SystemConfigTest extends TestCase
         $postAction = new SystemConfigPostAction(Factory::createAdminApiFactory()->getActionClientUtils());
 
         $postAction->postSystemConfig(new SystemConfigPostPayload([
-            'HeptaConnectPackageShopware6.config.testValue' => 'testReadDomainThatIsConfigurationKey',
+            self::$pluginName . '.config.testValue' => 'testReadDomainThatIsConfigurationKey',
         ]));
 
         $getAction = new SystemConfigGetAction(Factory::createAdminApiFactory()->getActionClientUtils());
 
         static::assertSame([], $getAction->getSystemConfig(
-            new SystemConfigGetCriteria('HeptaConnectPackageShopware6.config.testValue')
+            new SystemConfigGetCriteria(self::$pluginName . '.config.testValue')
         )->getValues());
+    }
+
+    private static function createExtensionFile(string $name): string
+    {
+        $file = \sys_get_temp_dir() . '/' . $name . '.zip';
+
+        $zipFile = new \ZipArchive();
+        $zipFile->open($file, \ZipArchive::CREATE);
+        $zipFile->addFromString($name . '/src/' . $name . '.php', '<?php namespace ' . $name . '; class ' . $name . ' extends \\Shopware\\Core\\Framework\\Plugin {}');
+        $zipFile->addFromString(
+            $name . '/src/Resources/config/config.xml',
+            <<<'XML'
+<?xml version="1.0" encoding="UTF-8"?>
+<config
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/shopware/platform/6.4.0.0/src/Core/System/SystemConfig/Schema/config.xsd"
+>
+    <card>
+        <input-field>
+            <name>testValue</name>
+            <label>Test Value</label>
+        </input-field>
+    </card>
+</config>
+XML
+        );
+        $zipFile->addFromString($name . '/composer.json', \json_encode([
+            'name' => 'heptaconnect-package/' . \mb_strtolower($name),
+            'description' => $name,
+            'version' => '1.0.0',
+            'type' => 'shopware-platform-plugin',
+            'license' => 'MIT',
+            'authors' => [
+                [
+                    'name' => 'HEPTAconnect Package',
+                    'role' => 'Manufacturer',
+                ],
+            ],
+            'extra' => [
+                'shopware-plugin-class' => $name . '\\' . $name,
+                'label' => [
+                    'de-DE' => $name,
+                    'en-GB' => $name,
+                ],
+                'description' => [
+                    'de-DE' => $name,
+                    'en-GB' => $name,
+                ],
+                'manufacturerLink' => [
+                    'de-DE' => 'http://' . $name . '.test',
+                    'en-GB' => 'http://' . $name . '.test',
+                ],
+                'supportLink' => [
+                    'de-DE' => 'http://' . $name . '.test',
+                    'en-GB' => 'http://' . $name . '.test',
+                ],
+            ],
+            'autoload' => [
+                'psr-4' => [
+                    $name . '\\' => 'src',
+                ],
+            ],
+        ]));
+
+        $zipFile->close();
+
+        return $file;
+    }
+
+    private static function createExtensionName(): string
+    {
+        return 'HeptaConnectExt' . \bin2hex(\random_bytes(8));
     }
 }
